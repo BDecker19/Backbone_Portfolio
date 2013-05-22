929ce94a60afd3a29aeb6451887bd46f67afa180
OrenBNV <orenbnv@gmail.com>
2013-05-21 21:43:34 -0400

    added rails to the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackbonePortfolio::Application.config.secret_token = 'ccb6c087ff3f1ce5e637946c6df94c90a45c4f16bfd53aa33a8c86cfccc6dc0cc76e07732632dd0f905a342458cb85fe7f0eb9af7b0cc315d73dcccf2ea1a6fe'
